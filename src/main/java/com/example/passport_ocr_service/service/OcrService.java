150ededfd91448a0a96a1b322be482353fd0aea5
2021-04-09500 <ndossalucent@gmail.com>
2026-01-12 02:11:23 +0300

    Added dockerfile

--- src/main/java/com/example/passport_ocr_service/service/OcrService.java
+++ src/main/java/com/example/passport_ocr_service/service/OcrService.java
@@ -1,32 +1,30 @@
 package com.example.passport_ocr_service.service;
 
-import lombok.extern.log4j.Log4j2;
+import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Service
 public class OcrService {
 
-    @Autowired
-    private Tesseract tesseract;
-
-    @Autowired
-    private ImagePreprocessService preprocessService;
-
     public String extractText(MultipartFile file) throws Exception {
-        File temp = File.createTempFile("passport", ".png");
-        file.transferTo(temp);
 
-        BufferedImage image = preprocessService.preprocess(temp);
-        return tesseract.doOCR(image);
+        ITesseract tesseract = new Tesseract();
+
+        // VERY IMPORTANT for Docker/Linux
+        tesseract.setDatapath("/usr/share/tesseract-ocr/4.00/tessdata");
+        tesseract.setLanguage("eng");
+
+        File tempFile = File.createTempFile("ocr-", ".png");
+        file.transferTo(tempFile);
+
+        try {
+            return tesseract.doOCR(tempFile);
+        } finally {
+            tempFile.delete();
+        }
     }
 }
